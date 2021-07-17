caa9f2fa1baa3dc9b4d546753e4b43346d04ea39
AbdallahGot <abdallah897107@gmail.com>
2021-07-18 01:42:49 +0300

    upload files Relates #1

---
+++ src/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Todo({ todo , toggleTodo }){
+    function handleTodoClick(){
+        toggleTodo(todo.id)
+    }
+    return (
+    
+        <>
+        <label>
+        <input type = "checkbox" checked = {todo.complete} onChange={handleTodoClick}/>
+        {todo.name}
+        </label>
+        
+        </>
+    );
+}
+export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Todo from './Todo';
 
-function TodoList(){
+export default function TodoList({todos ,toggleTodo}){
     return (
+        
+            todos.map( todo => {
+                return(
+                <div>
+                        < Todo key= {todo.id} toggleTodo= {toggleTodo} todo= {todo} />
+                </div>
+                )
+            })
         
     );
 }
-
-export default TodoList;
-
